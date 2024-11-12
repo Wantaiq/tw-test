0fbdfb4541776df018cbab08c7864fb0c649bbfc
wantiq <99611999+Wantaiq@users.noreply.github.com>
2024-11-12 07:11:29 -0800

    feature: add product filtering

---
+++ client/src/components/ProductFilter.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+  import useProductsStore from '@/stores/productsStore';
+  import { storeToRefs } from 'pinia';
+  const { products } = storeToRefs(useProductsStore());
+
+  const handleStockFilter = (event: Event) => {
+    const eventTarget = event.target as HTMLInputElement;
+    if (eventTarget.checked) {
+      products.value.filter(product => {
+        console.log(product.availabilityStatus === 'In Stock');
+        return product.availabilityStatus === 'In Stock';
+      });
+    } else {
+    }
+  };
+</script>
+
+<template>
+  <label for="showOnlyInStock">Show only available items</label>
+  <input
+    type="checkbox"
+    id="showOnlyInStock"
+    name="showOnlyStocked"
+    @change="handleStockFilter" />
+</template>
