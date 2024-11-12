7f276c437d3a0a97e04de292368f7646c6cb2ba0
wantiq <99611999+Wantaiq@users.noreply.github.com>
2024-11-12 02:27:13 -0800

    fix: remove credentials from api instance

--- client/src/utils/api.ts
+++ client/src/utils/api.ts
@@ -1,9 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://dummyjson.com/products',
+  baseURL: 'https://dummyjson.com/',
   timeout: 5000,
-  withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
   },
