d23568588d37636a4d51b2691983f40dcc89ec31
wantiq <99611999+Wantaiq@users.noreply.github.com>
2024-11-12 07:00:49 -0800

    feature: add base select component

---
+++ client/src/components/AppSelect.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+  const emit = defineEmits(['selected']);
+
+  const handleSelected = (event: Event) => {
+    const target = event.target as HTMLInputElement;
+    emit('selected', target.value);
+  };
+</script>
+
+<template>
+  <select
+    @change="handleSelected"
+    class="text-neutral-900">
+    <slot name="options" />
+  </select>
+</template>
