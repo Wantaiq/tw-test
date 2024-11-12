6c18df8525e4b9cc16d1c6b1cadec89f5c2f8eff
wantiq <99611999+Wantaiq@users.noreply.github.com>
2024-11-12 01:03:00 -0800

    feature: add default app card

---
+++ client/src/components/AppCard.vue
@@ -0,0 +1,24 @@
+<template>
+  <div
+    class="flex flex-col gap p-6 rounded-lg bg-neutral-800 border border-neutral-700
+      md:max-w-80">
+    <div v-if="$slots.image">
+      <slot name="image" />
+    </div>
+    <div
+      v-if="$slots.title"
+      class="mt-2">
+      <slot name="title" />
+    </div>
+    <div
+      v-if="$slots.body"
+      class="mt-2">
+      <slot name="body" />
+    </div>
+    <div
+      v-if="$slots.footer"
+      class="mt-2">
+      <slot name="footer" />
+    </div>
+  </div>
+</template>
