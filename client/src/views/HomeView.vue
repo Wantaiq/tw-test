2faa83aca35907c86873fa5fd630a8622e8e2545
wantiq <99611999+Wantaiq@users.noreply.github.com>
2024-11-12 07:04:24 -0800

    fix: reactivity of products

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,5 +1,36 @@
+<script setup lang="ts">
+  import ProductFilter from '@/components/ProductFilter.vue';
+  import ProductGrid from '@/components/ProductGrid.vue';
+  import ProductSort from '@/components/ProductSort.vue';
+  import useProducts from '@/composables/useProducts';
+  import useProductsStore from '@/stores/productsStore';
+  import { storeToRefs } from 'pinia';
+  import { watch } from 'vue';
+  const { data } = useProducts();
+  const { products } = storeToRefs(useProductsStore());
+
+  watch(data, newData => {
+    if (newData) {
+      products.value = newData;
+    }
+  });
+
+  const handleSort = (value: string) => {
+    if (value === 'prAsc') {
+      products.value = [...products.value].sort((a, b) => a.price - b.price);
+    } else {
+      products.value = [...products.value].sort((a, b) => a.price + b.price);
+    }
+  };
+</script>
+
 <template>
-  <main>
-    <h1>Home View</h1>
-  </main>
+  <ProductFilter />
+  <ProductSort @selected-option="handleSort" />
+  <Suspense>
+    <ProductGrid
+      v-if="products.length"
+      :products="products" />
+    <template #fallback>Loading...</template>
+  </Suspense>
 </template>
